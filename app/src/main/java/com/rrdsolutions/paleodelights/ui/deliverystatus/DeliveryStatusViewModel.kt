8edd87b7c4540a0171704d190aa94fd945f7ec97
Rinor <rinorramlifreelance@gmail.com>
2020-05-28 10:42:55 +0800

    Completed
    Signed-off-by: Rinor <rinorramlifreelance@gmail.com>

--- app/src/main/java/com/rrdsolutions/paleodelights/ui/deliverystatus/DeliveryStatusViewModel.kt
+++ app/src/main/java/com/rrdsolutions/paleodelights/ui/deliverystatus/DeliveryStatusViewModel.kt
@@ -38,7 +38,7 @@ class DeliveryStatusViewModel: ViewModel() {
                         for (document in documents){
                             val order = OrderModel.Order(
                                 document.id,
-                                document.data["  phonenumber"] as String,
+                                document.data["phonenumber"] as String,
                                 document.data["time"] as String,
                                 document.data["eta"] as String,
                                 document.data["itemlist"] as List<String>,
